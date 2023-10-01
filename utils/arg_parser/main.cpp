60f6e9b207d70252a7e2ea936fa36d67565ae1a1
animit <animit30@gmail.com>
2023-10-01 16:08:22 +0000

    argument parser homework

--- utils/arg_parser/main.cpp
+++ utils/arg_parser/main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char** argv)
     arg_parser parser(argc, argv, "cdf:r:");
     for (auto it = parser.begin(); it != parser.end(); ++it)
     {
-        auto taken = (*it)._value;
         std::cout << (*it)._key << " " << ((*it)._value ? (*it)._value.value() : "" ) << std::endl;
     }
     return 0;

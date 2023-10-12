88f60e1ae9c265dc3d0b9fd466264c55516290dc
Ani Mitoyan <97410497+animittt@users.noreply.github.com>
2023-10-12 21:32:31 +0400

    logger attempt (#2)
    * logger attempt
    
    * logger attempt 2
    
    * added #pragma once
    
    * added macros for easier logging
    
    * added macros for easier logging

---
+++ utils/logging/logger.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#define LOG(level, message) Log(level, message, __FILE__, __func__, __LINE__)
+#define logging_message "[" << std::put_time(timeInfo, "%T") << "] " << "[" << FILE << ":" << FUNC << ":" << LINE << "]" << " [" << LogLevelStr(level) << "] "<< message
+
+enum  logLevel
+{
+    INFO, WARNING, ERROR, FATAL, DEBUG
+};
+
+class Logger
+{
+private:
+    std::ostream& out;
+    std::ofstream outf;
+    bool colored;
+
+public:
+    explicit Logger(const char* filename) :
+    outf(filename)
+    , out(filename ? outf : std::cout)
+    , colored((filename == nullptr))
+    {
+    }
+    ~Logger()
+    {
+        out << std::flush;
+        outf.close();
+    }
+    static std::string LogLevelStr(logLevel level)
+    {
+        switch (level)
+        {
+            case INFO:
+                return "INFO";
+            case WARNING:
+                return "WARNING";
+            case ERROR:
+                return "ERROR";
+            case FATAL:
+                return "FATAL";
+            case DEBUG:
+                return "DEBUG";
+            default:
+                return "UNKNOWN";
+        }
+    }
+    void Log(logLevel level,const std::string& message, const std::string& FILE, const std::string& FUNC, int LINE )
+    {
+        std::time_t currentTime = std::time(nullptr);
+        std::tm* timeInfo = std::localtime(&currentTime);
+
+        if (colored)
+        {
+            out << getColor(level)
+                << logging_message
+                << getColor(logLevel::INFO) <<'\n';
+        }
+        else
+            out << logging_message << '\n';
+    }
+    static char const* getColor(logLevel level)
+    {
+        switch(level)
+        {
+            case INFO:
+                return "\033[36m";
+            case ERROR:
+                return "\033[31m";
+            case WARNING:
+                return "\033[33m";
+            case DEBUG:
+                return "\033[34m";
+            case FATAL:
+                return "\033[31;1;4m";
+            default:
+                return "\033[0m";
+        }
+    }
+};
+

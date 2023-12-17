1bf375d534cec8cab878299c01eb5f995ecd4e47
animit <animit30@gmail.com>
2023-12-17 12:33:21 +0000

    changed the code to avoid using const_cast

--- do_command/do_command.cpp
+++ do_command/do_command.cpp
@@ -10,14 +10,30 @@ int do_command(const char* argv[])
     pid_t child_pid = fork();
     if (child_pid == -1)
     {
-        LOG_FATAL(nullptr, "Fork failed" + std::string(strerror(errno)));
-        return -1;
+        LOG_FATAL("Fork failed" + std::string(strerror(errno)));
     }
     else if (child_pid == 0)
     {
-        execvp(argv[0], const_cast<char* const*>(argv));
-        LOG_ERROR(nullptr, "execvp error" + std::string(strerror(errno)));
-        exit(EXIT_FAILURE);
+        int count = 0;
+        while (argv[count] != nullptr)
+        {
+            ++count;
+        }
+
+        char** exec_args = new char*[count + 1];
+        for (int i = 0; i < count; ++i)
+        {
+            exec_args[i] = strdup(argv[i]);
+        }
+        exec_args[count] = nullptr;
+
+        execvp(exec_args[0], exec_args);
+        for (int i = 0; i < count; ++i)
+        {
+            free(exec_args[i]);
+        }
+        delete[] exec_args;
+        LOG_FATAL("execvp error" + std::string(strerror(errno)));
     }
     else
     {
@@ -29,27 +45,24 @@ int do_command(const char* argv[])
         }
         else
         {
-            LOG_FATAL(nullptr, "Child process didn't terminate normally" + std::string(strerror(errno)));
-            return -1;
+            LOG_FATAL("Child process didn't terminate normally" + std::string(strerror(errno)));
         }
     }
+    return -1;
 }
 
 int main()
 {
-    const char* program1[4];
-    program1[0] = "ls";
-    program1[1] = "-a";
-    program1[2] = "-l";
-    program1[3] = nullptr;
-    
-    const char* program2[3];
-    program2[0] = "cat";
-    program2[1] = "/etc/lsb-release";
-    program2[2] = nullptr;
-
-    int exit_code1 = do_command(program1);
-    int exit_code2 = do_command(program2);
+    const char* program[4];
+    program[0] = "ls";
+    program[1] = "-a";
+    program[2] = "-l";
+    program[3] = nullptr;
+    int exit_code1 = do_command(program);
+    program[0] = "cat";
+    program[1] = "/etc/lsb-release";
+    program[2] = nullptr;
+    int exit_code2 = do_command(program);
     std::cout << "Exit code 1: " << exit_code1 << '\n';
     std::cout << "Exit code 2: " << exit_code2 << '\n';
     return 0;

01de2fa9f057c485f5c347ac72443409f1e52eb7
animit <animit30@gmail.com>
2024-03-29 14:04:41 +0000

    now using siginfo_t in commissioner

---
+++ hw17_back_to_the_ussr/commissioner.cpp
@@ -0,0 +1,78 @@
+// commissioner.cpp
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <unistd.h>
+#include <sys/wait.h>
+
+bool responded = false;
+
+void handle_spy(pid_t pid)
+{
+    std::cout << "Spionen of bourjois, western, anticommunist, visual studio/windows using scum detected with PID: " << pid << std::endl;
+    std::cout << "Sending SIGKILL to terminate the spy process.\n";
+    kill(pid, SIGKILL);
+}
+
+void handle_response(int signal)
+{
+    if (signal == SIGILL)
+    {
+        std::cout << "Received response from the spy process.\n";
+        responded = true;
+    }
+}
+
+void send_signal_to_spy(int pid)
+{
+    std::cout << "Sending SIGCONT to a process with PID: " << pid << std::endl;
+    kill(pid, SIGCONT);
+}
+
+int main()
+{
+    std::ifstream file("postman.txt");
+    std::vector<pid_t> pids;
+
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open file 'postman.txt'. Exiting.\n";
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        pid_t pid;
+        while (iss >> pid)
+            pids.push_back(pid);
+    }
+    file.close();
+
+    std::cout << "Checking for spy processes...\n";
+    struct sigaction sa{};
+    sa.sa_handler = handle_response;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGILL, &sa, nullptr);
+
+    for (pid_t pid : pids)
+    {
+        std::string procPath = "/proc/" + std::to_string(pid);
+        if (access(procPath.c_str(), F_OK) == 0)
+        {
+            std::cout << "Checking process with PID: " << pid << std::endl;
+            send_signal_to_spy(pid);
+            usleep(10000);
+            if(responded)
+                handle_spy(pid);
+            responded = false;
+        }
+    }
+
+    std::cout << "All spy processes handled.\n";
+    return 0;
+}
